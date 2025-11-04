8f2278f3d8ea98f2ae3bb1b581cfd39923b6f75c
DariusDunk <gorski4ovek@abv.bg>
2025-11-04 21:41:56 +0200

    Added methods for equals and hashcode, through Lombok annotations inside CustomerCartId class.

--- src/main/java/com/example/ecomerseapplication/CompositeIdClasses/CustomerCartId.java
+++ src/main/java/com/example/ecomerseapplication/CompositeIdClasses/CustomerCartId.java
@@ -5,6 +5,7 @@ import com.example.ecomerseapplication.Entities.Product;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import java.io.Serializable;
@@ -12,6 +13,7 @@ import java.io.Serializable;
 @Embeddable
 @Getter
 @NoArgsConstructor
+@EqualsAndHashCode
 public class CustomerCartId implements Serializable {
 
     @JoinColumn(name = "product_id")
