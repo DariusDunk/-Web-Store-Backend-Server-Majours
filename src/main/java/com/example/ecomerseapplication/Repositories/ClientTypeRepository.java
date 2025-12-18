c3f423c5677af9f12ee64ecae27ad58d0409a267
DariusDunk <gorski4ovek@abv.bg>
2025-12-18 20:09:24 +0200

    - Created services and repositories for the ClientType and Session entitites.

---
+++ src/main/java/com/example/ecomerseapplication/Repositories/ClientTypeRepository.java
@@ -0,0 +1,7 @@
+package com.example.ecomerseapplication.Repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientTypeRepository <ClientType, Long>{
+}
---
+++ src/main/java/com/example/ecomerseapplication/Services/SessionService.java
@@ -0,0 +1,7 @@
+package com.example.ecomerseapplication.Services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SessionService {
+}
