25a303cf67f0bf9bf011bc739f919225ea78118a
DariusDunk <gorski4ovek@abv.bg>
2026-03-29 21:21:17 +0300

    feat: replace fetch with axios for getFilters endpoint in attributeRoutes.js
    - Replaced fetch with axios for getFilters endpoint in attributeRoutes.js.
    - Removed redundant commented-out code for improved readability.
    - Enhanced error handling and logging for backend errors.

--- src/main/nodeJs/browserBFF/routes/attributeRoutes.js
+++ src/main/nodeJs/browserBFF/routes/attributeRoutes.js
@@ -1,41 +1,22 @@
 const express =require( 'express');
 const router = express.Router();
 const { Backend_Url } = require('./config.js');
+const {get} = require("axios");
 
 router.get('/getFilters/:categoryName', async (req, res)=>{
 
-  const queryParts = req.url.split("/");
-  const categoryName = queryParts[2];
-  // console.log(`category name: ${categoryName}`)
+  const categoryName = req.params.categoryName
+
   try
   {
-    const response = await fetch(`${Backend_Url}/category/filters?categoryName=${categoryName}`,
-      {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(res.body)
-      });
-
-    // const text = await response.text();
-
-    const responseData = await response.json();
-
-    // let responseData = null
-
-    if (!response.ok) {
-      return res.status(response.status).end();
-    }
-
-    // console.log("responseData: " + JSON.stringify(responseData))
+      const response = await get(`${Backend_Url}/category/filters?categoryName=${categoryName}`, {})
+      const responseData = response.data;
 
     return res.status(response.status).json(responseData || {})
   }catch (error) {
-    console.error('Error:', error);
-    return res.status(500).json({ error: 'Internal server error' });
+    console.error('-------------------Error getting category filters\n', error);
+    return res.status(500).end();
   }
-
 })
 
 module.exports = router
